56c3c6f00d90f7e11196d3a95d74429c0c3acac7
Thomaskoooo <tomas.kocur@spsehalova.sk>
2024-05-06 14:48:45 +0200

    Full Kalkulacka
    Plus, minus, krat, deleno, odmocniny, mocniny, log

---
+++ c#/kalkulacka/kalkulacka/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace kalkulacka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = firstnumber + secondnumber;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = firstnumber - secondnumber;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = firstnumber * secondnumber;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = firstnumber / secondnumber;
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = Math.Pow(firstnumber, secondnumber);
+            textBox3.Text = Convert.ToString(result);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = Math.Sqrt(firstnumber + secondnumber);
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = Math.Sin(firstnumber + secondnumber);
+            textBox3.Text = Convert.ToString(result);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            double firstnumber = Convert.ToDouble(textBox1.Text);
+            double secondnumber = Convert.ToDouble(textBox2.Text);
+            double result = Math.Log(firstnumber + secondnumber);
+            textBox3.Text = Convert.ToString(result);
+        }
+    }
+}
